456233cd4789e8212ad4a96312d3e54f4b067f39
Gaelen McIntee <mcinteeg@mail.gvsu.edu>
2017-02-13 23:59:25 -0500

    Working lockstep synchronization

--- lab5-sharedMem/reader.c
+++ lab5-sharedMem/reader.c
@@ -11,9 +11,11 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-#include <unistd.h> //debug
+#include <unistd.h>
 
 #define SHM_SIZE 4096
+#define ME 1
+#define OTHER 0
 
 void quit(int signum);
 
@@ -23,11 +25,12 @@ char *shmAddr;
 
 int main(int argc, char *argv[])
 {
-	int turn;
+	int *turn;		// flags for keeping track of the current turn
+	char *text;		// the message that will be passed to readers
 	signal(SIGINT, quit);
 
 	// Set Up –––––––––––––––––––––––––––
-	shmKey = ftok("writer", 'A'); //use executable for the writer as file
+	shmKey = ftok("writer", 'A'); // use executable for the writer as file key
 	if (shmKey == -1) {
 		perror("ftok()");
 		exit(1);
@@ -39,20 +42,48 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	shmAddr = shmat(shmId, NULL, 0);
+	shmAddr = shmat(shmId, NULL, 0);  // no addr or flg because we expect
 	if (shmAddr == (void *) -1) {
 		perror("shmat()");
 		exit(1);
 	}
 
+	turn = (int *)shmAddr;				// Put turn array at beginning of shm
+	text = shmAddr + (sizeof(int)*2);	// Put text after turn array
+	turn[ME] = 0;		// writer goes first
 
 	while (1)
 	{
-		// Lockstep Synchronization goes here... idk how to do it tho
+		/*
+		The value turn[ME] is incrememnted for every reader process that
+		enters the CS. When a reader leaves the CS, turn[ME] is decrememnted.
+		turn[ME] must be zero before turn[OTHER] can be set to 1.
+	 	0 = Not in CS; >0 = In CS
+		*/
 
-		// Critical Section ––––––––––––––––––––
-		fprintf(stderr, "Shared mem: %s", shmAddr);
-		//End Critical Section ––––––––––––––––
+		while(turn[OTHER] == 1)
+			;
+
+		// usleep prevents concurrency issues, ensures every reader actually
+		// performs a decremement
+		usleep(1);
+		turn[ME]++;
+
+		// Critical Section –––––––––––––––––––
+		fprintf(stderr, "Read from shared mem: %s\n", text);
+		// End Critical Section –––––––––––––––
+
+		usleep(1);
+		turn[ME]--;
+
+		//fprintf(stderr, "Turns: Me:%i  Other:%i\n", turn[ME], turn[OTHER]);  //debug
+		//sleep(3);  //debug
+
+		// Block readers that have already finished from pre-emptively setting
+		// the writer's turn
+		while(turn[ME] > 0)
+		  	;
+		turn[OTHER] = 1;
 	}
 
 }
@@ -67,7 +98,7 @@ void quit(int signum)
 		exit(1);
 	}
 
-	//reader does not mark shared mem for deletion... i think
+	// reader does not mark shared mem for deletion... i think
 
 	exit(0);
 }
--- lab5-sharedMem/writer.c
+++ lab5-sharedMem/writer.c
@@ -11,9 +11,11 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-#include <unistd.h> //debug
+#include <unistd.h>
 
 #define SHM_SIZE 4096
+#define ME 0
+#define OTHER 1
 
 void quit(int signum);
 
@@ -25,17 +27,19 @@ int main(int argc, char *argv[])
 {
 
 	char input[SHM_SIZE];
-	int turn;
+	int *turn;		// flags for keeping track of the current turn
+	char *text;		// the message that will be passed to readers
 	signal(SIGINT, quit);
 
 	// Set Up –––––––––––––––––––––––––––
-	shmKey = ftok("writer", 'A');
+	shmKey = ftok("writer", 'A'); // use executable for the writer as file key
 	if (shmKey == -1) {
 		perror("ftok()");
 		exit(1);
 	}
 
-	shmId = shmget(shmKey, SHM_SIZE, IPC_CREAT|IPC_EXCL|0600);
+	// create a shm of size SHM_SIZE plus the size of 2 ints (for flags)
+	shmId = shmget(shmKey, ( SHM_SIZE+(sizeof(int)*2) ), IPC_CREAT|IPC_EXCL|0600);
 	if (shmId < 0) {
 		perror("shmget()");
 		exit(1);
@@ -47,24 +51,26 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	turn = (int *)shmAddr;				//Put turn array at beginning of shm
+	text = shmAddr + (sizeof(int)*2);	//Put text after turn array
+	turn[ME] = 1;		// writer goes first
 
 	while (1)
 	{
-		// Lockstep Synchronization goes here... idk how to do it tho
-
-		turn = 0;
-		while(turn != 0)
+		// The writer is blocked when there are any readers in the critical Section
+		// (when turn[OTEHR] > 0) or it's not the writer's turn.
+		while(turn[OTHER] > 0 || turn[ME] == 0)
 			;
+
 		// Critical Section ––––––––––––––––––––
 		fprintf(stderr, "Enter a string: ");
 		fgets(input, SHM_SIZE, stdin);
+		strcpy(text, input);
+		// End Critical Section ––––––––––––––––
 
-		strcpy(shmAddr, input);
-		// End Critical Section –––––––––––––––––
-
-		turn = 1;
-		//fprintf(stderr, "Shared mem: %s", shmAddr);  // debug
-		//sleep(10); //for checking ipcs at runtime
+		//fprintf(stderr, "Turns: Me:%i  Other:%i\n", turn[ME], turn[OTHER]);  // debug
+		//sleep(3); //for checking ipcs at runtime
+		turn[ME] = 0;
 	}
 
 }
